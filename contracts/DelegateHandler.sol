107cfb6e0569a21293ec0b98b9fea1c8e285e683
romaqQ <schick.roman@outlook.com>
2023-10-05 18:30:24 +0200

    change DelegateHanlder signatureEncoding type

--- contracts/DelegateHandler.sol
+++ contracts/DelegateHandler.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 contract DelegateHandler {
-    function delegateExecuteDCA(address dcaContract, uint256 amountInETH) external {
+    function delegateExecuteDCA(address dcaContract, uint256[] calldata amountsInETH) external {
         (bool success, ) = dcaContract.call(
-            abi.encodeWithSignature("executeDCA(address,uint256)", msg.sender, amountInETH)
+            abi.encodeWithSignature("executeDCA(address,uint256[])", msg.sender, amountsInETH)
         );
         require(success, "DCA failed");
     }
